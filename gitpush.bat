925875599d9125de59043a909a9590578ab06fb9
rweil55 <github@shaw-weil.com>
2025-01-03 09:51:40 -0500

    from indesign 2025-01-03 14:51

--- gitpush.bat
+++ gitpush.bat
@@ -3,7 +3,7 @@ cd /inetpub/wwwroot/w/mctrail
 
 
 git add .
-git commit -m "from indesign 2025-01-03 14:32" --no-edit
+git commit -m "from indesign 2025-01-03 14:51" --no-edit
 git push https://github.com/rweil55/web-mctrail main
 
 rem wait for one minute to allow git hub to get its act together
