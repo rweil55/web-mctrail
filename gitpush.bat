89544c1d2726fd5d3e7a7bb9b98b80b211332315
rweilwww <github@shaw-weil.com>
2025-10-26 11:21:50 -0400

    from inDesign 2025-10-26 15:18

--- gitpush.bat
+++ gitpush.bat
@@ -3,11 +3,10 @@ cd /inetpub/wwwroot/w/mctrail
 
 
 git add .
-git commit -m "from indesign 2025-08-23 01:27" --no-edit
+git commit -m "from inDesign 2025-10-26 15:18" --no-edit
 git push --force --set-upstream origin main
 
 rem wait for one minute to allow git hub to get its act together
 timeout 60
-rem go fo it!!
 timeout 5
 exit
